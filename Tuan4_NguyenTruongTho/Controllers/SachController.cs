37c937bca7c1d55fbb09d17199c73841041c56c3
thont <thont@gmail.com>
2022-03-16 14:02:19 +0700

    BaiTapLapTrinhWeb

---
+++ Tuan4_NguyenTruongTho/Controllers/SachController.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Tuan4_NguyenTruongTho.Models;
+using PagedList;
+
+namespace Tuan4_NguyenTruongTho.Controllers
+{
+    public class SachController : Controller
+    {
+        // GET: Sach
+        MyDataDataContext data = new MyDataDataContext();
+        public ActionResult ListSach()
+        {
+            var all_sach = from ss in data.Saches select ss;
+            return View(all_sach);
+        }
+        public ActionResult Detail(int id)
+        {
+            var D_sach = data.Saches.Where(m => m.masach == id).First();
+            return View(D_sach);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(FormCollection collection, Sach s)
+        {
+            var E_ten = collection["ten"];
+            var E_mota = collection["mota"];
+            var E_hang = collection["hang"];
+            var E_gia = Convert.ToDecimal(collection["giaban"]);
+            var E_hinh = collection["hinh"];
+            var E_ngaycapnhat = Convert.ToDateTime(collection["ngaycapnhat"]);
+            var E_soluongton = Convert.ToInt32(collection["soluongton"]);
+            if (string.IsNullOrEmpty(E_ten))
+            {
+                ViewData["Error"] = "Don't empty!";
+            }
+
+            else
+            {
+                s.tensach = E_ten.ToString();
+                s.hinh = E_hinh.ToString();
+                s.giaban = E_gia;
+                s.ngaycapnhat = E_ngaycapnhat;
+                s.soluongton = E_soluongton;
+                data.Saches.InsertOnSubmit(s);
+                data.SubmitChanges();
+                return RedirectToAction("Index");
+            }
+            return this.Create();
+        }
+
+        public string ProcessUpload(HttpPostedFileBase file)
+        {
+            if (file == null)
+            {
+                return "";
+            }
+            file.SaveAs(Server.MapPath("~/Content/images/" + file.FileName));
+            return "/Content/images/" + file.FileName;
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var E_sach = data.Saches.First(m => m.masach == id);
+            return View(E_sach);
+        }
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            var E_sach = data.Saches.First(m => m.masach == id);
+            var E_ten = collection["ten"];
+            var E_mota = collection["mota"];
+            var E_hang = collection["hang"];
+            var E_gia = Convert.ToDecimal(collection["giaban"]);
+            var E_hinh = collection["hinh"];
+            var E_ngaycapnhat = Convert.ToDateTime(collection["ngaycapnhat"]);
+            var E_soluongton = Convert.ToInt32(collection[" soluongton"]);
+            E_sach.masach = id;
+            if (string.IsNullOrEmpty(E_ten))
+            {
+                ViewData["Error"] = "Don 't empty!";
+            }
+            else
+            {
+                E_sach.tensach = E_ten;
+                E_sach.hinh = E_hinh;
+                E_sach.giaban = E_gia;
+                E_sach.ngaycapnhat = E_ngaycapnhat;
+                E_sach.soluongton = E_soluongton;
+                UpdateModel(E_sach);
+                data.SubmitChanges();
+                return RedirectToAction("ListSach");
+            }
+            return this.Edit(id);
+        }
+        public ActionResult Delete(int id)
+        {
+            var D_sach = data.Saches.First(m => m.masach == id);
+            return View(D_sach);
+        }
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            var D_sach = data.Saches.Where(m => m.masach == id).First();
+            data.Saches.DeleteOnSubmit(D_sach);
+            data.SubmitChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
